d1154cdfb09da00a379ef04eec06226423ba9e46
Onabil97971 <Onabil97971@yahoo.com>
2022-09-30 20:49:52 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,27 @@
+const tabItems = document.querySelectorAll(".tab-item")
+const tabContentItems = document.querySelectorAll(".tab-content-item")
+
+// Select for tab item
+function selectItem(e) {
+    removeBorder();
+    removeShow();
+    // add border to the tab
+    this.classList.add("tab-border")
+    // grab content item from dom
+    const tabContentItem = document.querySelector(`#${this.id}-content`)
+    // add show class
+    tabContentItem.classList.add('show')
+}
+
+
+function removeBorder() {
+    tabItems.forEach(item => item.classList.remove('tab-border'))
+}
+
+
+function removeShow() {
+    tabContentItems.forEach(item => item.classList.remove('show'))
+}
+
+// Listen for tab click
+tabItems.forEach(item => item.addEventListener('click', selectItem))
